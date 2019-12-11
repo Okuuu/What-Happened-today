d04fa6ab5dcf7e6e4d0b50766e33916122ca99a3
Robin <robin.mesa.marin@gmail.com>
2019-12-11 13:06:28 +0100

    Add range slider

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const simpleDate = {
 
 
 function formatDate(integer) {
-  if(integer.toString().length < 2) {
+  if (integer.toString().length < 2) {
     return '0' + integer
   } else {
     return integer
@@ -30,7 +30,7 @@ const header = new Vue({
       }
       this.title = newDate.day + '/' + newDate.month;
       events.getEvents(newDate.month, newDate.day);
-    }
+    },
   }
 })
 
@@ -38,26 +38,79 @@ const events = new Vue({
   el: '#events',
   data() {
     return {
-      info: null
+      info: null,
+      length: null,
+      fullInfo: null
     }
   },
   mounted() {
     this.getEvents(simpleDate.month, simpleDate.day);
   },
   methods: {
-    getEvents: function (month, day) {
+
+    getEvents(month, day) {
       axios
         .get('https://byabbe.se/on-this-day/' + month + '/' + day + '/events.json')
-        .then(response => (this.info = response.data.events))
-    }
+        .then(response => (
+          this.info = response.data.events,
+          this.fullInfo = response.data.events,
+          this.length = response.data.events.length,
+          this.sliderInit()))
+
+    },
+    sliderInit() {
+      const value = {
+        min: this.info[0].year,
+        max: this.info[this.info.length - 1].year
+      };
+
+      const eventSlider = document.querySelector('#sliderEvent');
+      const format = wNumb({
+        decimals: 0
+      });
+      const slider = noUiSlider.create(eventSlider, {
+        start: [parseInt(value.min, 10), parseInt(value.max, 10)],
+        connect: true,
+        margin: 5,
+        tooltips: [format, format],
+        pips: {
+          mode: 'steps',
+          density: 5,
+          format: format
+        },
+        range: {
+          min: parseInt(value.min, 10),
+          max: parseInt(value.max, 10)
+        }
+      })
+
+      const update = this.updateData;
+      eventSlider.noUiSlider.on('end', function (event) {
+        const sliderValue = {
+          start: Math.round(event[0]),
+          end: Math.round(event[1])
+        }
+        update(sliderValue);
+      });
+    },
+    updateData(sliderValue) {
+      this.info = this.fullInfo.filter(function (event) {
+        return event.year >= sliderValue.start && event.year <= sliderValue.end
+      })
+      this.length = this.info.length;
+    },
   }
 })
+
+
 
 const births = new Vue({
   el: '#births',
   data() {
     return {
-      info: null
+      info: null,
+      fullInfo: null,
+      length: null
     }
   },
   mounted() {
@@ -67,7 +120,52 @@ const births = new Vue({
     getBirths: function (month, day) {
       axios
         .get('https://byabbe.se/on-this-day/' + month + '/' + day + '/births.json')
-        .then(response => (this.info = response.data.births))
+        .then(response => (
+          this.info = response.data.births,
+          this.fullInfo = response.data.births,
+          this.length = response.data.births.length,
+          this.sliderInit()))
+    },
+    sliderInit() {
+      const value = {
+        min: this.info[0].year,
+        max: this.info[this.info.length - 1].year
+      };
+
+      const eventSlider = document.querySelector('#sliderBirths');
+      const format = wNumb({
+        decimals: 0
+      });
+      const slider = noUiSlider.create(eventSlider, {
+        start: [parseInt(value.min, 10), parseInt(value.max, 10)],
+        connect: true,
+        margin: 5,
+        tooltips: [format, format],
+        pips: {
+          mode: 'steps',
+          density: 5,
+          format: format
+        },
+        range: {
+          min: parseInt(value.min, 10),
+          max: parseInt(value.max, 10)
+        }
+      })
+
+      const update = this.updateData;
+      eventSlider.noUiSlider.on('end', function (event) {
+        const sliderValue = {
+          start: Math.round(event[0]),
+          end: Math.round(event[1])
+        }
+        update(sliderValue);
+      });
+    },
+    updateData(sliderValue) {
+      this.info = this.fullInfo.filter(function (event) {
+        return event.year >= sliderValue.start && event.year <= sliderValue.end
+      })
+      this.length = this.info.length;
     }
   }
 })
@@ -76,7 +174,9 @@ const deaths = new Vue({
   el: '#deaths',
   data() {
     return {
-      info: null
+      info: null,
+      fullInfo:null,
+      length: null
     }
   },
   mounted() {
@@ -86,7 +186,52 @@ const deaths = new Vue({
     getDeaths: function (month, day) {
       axios
         .get('https://byabbe.se/on-this-day/' + month + '/' + day + '/deaths.json')
-        .then(response => (this.info = response.data.deaths))
+        .then(response => (
+          this.info = response.data.deaths,
+          this.fullInfo = response.data.deaths,
+          this.length = response.data.deaths.length,
+          this.sliderInit()))
+    },
+    sliderInit() {
+      const value = {
+        min: this.info[0].year,
+        max: this.info[this.info.length - 1].year
+      };
+
+      const eventSlider = document.querySelector('#sliderDeaths');
+      const format = wNumb({
+        decimals: 0
+      });
+      const slider = noUiSlider.create(eventSlider, {
+        start: [parseInt(value.min, 10), parseInt(value.max, 10)],
+        connect: true,
+        margin: 5,
+        tooltips: [format, format],
+        pips: {
+          mode: 'steps',
+          density: 5,
+          format: format
+        },
+        range: {
+          min: parseInt(value.min, 10),
+          max: parseInt(value.max, 10)
+        }
+      })
+
+      const update = this.updateData;
+      eventSlider.noUiSlider.on('end', function (event) {
+        const sliderValue = {
+          start: Math.round(event[0]),
+          end: Math.round(event[1])
+        }
+        update(sliderValue);
+      });
+    },
+    updateData(sliderValue) {
+      this.info = this.fullInfo.filter(function (event) {
+        return event.year >= sliderValue.start && event.year <= sliderValue.end
+      })
+      this.length = this.info.length;
     }
   }
 })
