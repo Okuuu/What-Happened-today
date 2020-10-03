8b231efb156ae9b9de0515bd755b13845dd8bbf2
Martin <martin.r.matousek@gmail.com>
2020-10-03 11:59:51 +0200

    Added support for B.C. years

--- index.js
+++ index.js
@@ -3,36 +3,41 @@ const simpleDate = {
   day: date.getDate(),
   month: date.getMonth() + 1, // add +1 because months start at 0 like any array
   year: date.getFullYear()
-}
-const dateFormatOption = {
-  month: 'long',
-  day: '2-digit',
-}
+};
 
-function formatDate(integer) {
-  if (integer.toString().length < 2) {
-    return '0' + integer
+const dateFormatOption = {
+  month: "long",
+  day: "2-digit"
+};
+
+const sliderSettings = {};
+
+function beforeChristParse(value) {
+  // If the year seems B.C., return the same but negative value
+  if (new RegExp(/BC/).test(value)) {
+    return -parseInt(value, 10);
   } else {
-    return integer
+    return parseInt(value, 10);
   }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   const options = {};
-  const elems = document.querySelectorAll('.collapsible');
+  const elems = document.querySelectorAll(".collapsible");
   const instances = M.Collapsible.init(elems, options);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  const options = {format: 'mmmm dd'};
-  const elems = document.querySelectorAll('.datepicker');
+document.addEventListener("DOMContentLoaded", function () {
+  const options = { format: "mmmm dd" };
+  const elems = document.querySelectorAll(".datepicker");
   const instances = M.Datepicker.init(elems, options);
 });
 
+// Header
 const header = new Vue({
-  el: '#header',
+  el: "#header",
   data: {
-    inputDate: date.toLocaleDateString('en-EN', dateFormatOption)
+    inputDate: date.toLocaleDateString("en-EN", dateFormatOption)
   },
   methods: {
     changeDate: function () {
@@ -40,99 +45,113 @@ const header = new Vue({
 
       const newDate = {
         day: date.getDate(),
-        month: date.getMonth()+1
-      }
+        month: date.getMonth() + 1
+      };
 
       events.getEventsWithoutSliders(newDate.month, newDate.day);
       births.getBirthsWithoutSliders(newDate.month, newDate.day);
       deaths.getDeathsWithoutSliders(newDate.month, newDate.day);
-    },
+    }
   }
-})
+});
 
+// Events
 const events = new Vue({
-  el: '#events',
+  el: "#events",
   data() {
     return {
       info: null,
       length: null,
       fullInfo: null
-    }
+    };
   },
   mounted() {
     this.getEvents(simpleDate.month, simpleDate.day);
   },
   methods: {
-
     getEvents(month, day) {
       axios
-        .get('https://byabbe.se/on-this-day/' + month + '/' + day + '/events.json')
-        .then(response => (
-          this.info = response.data.events,
-          this.fullInfo = response.data.events,
-          this.length = response.data.events.length,
-          this.sliderInit()))
+        .get(
+          "https://byabbe.se/on-this-day/" + month + "/" + day + "/events.json"
+        )
+        .then(
+          (response) => (
+            (this.info = response.data.events),
+            (this.fullInfo = response.data.events),
+            (this.length = response.data.events.length),
+            this.sliderInit()
+          )
+        );
     },
     getEventsWithoutSliders(month, day) {
       axios
-        .get('https://byabbe.se/on-this-day/' + month + '/' + day + '/events.json')
-        .then(response => (
-          this.info = response.data.events,
-          this.fullInfo = response.data.events,
-          this.length = response.data.events.length))
+        .get(
+          "https://byabbe.se/on-this-day/" + month + "/" + day + "/events.json"
+        )
+        .then(
+          (response) => (
+            (this.info = response.data.events),
+            (this.fullInfo = response.data.events),
+            (this.length = response.data.events.length)
+          )
+        );
     },
     sliderInit() {
       const value = {
-        min: this.info[0].year,
-        max: this.info[this.info.length - 1].year
+        min: beforeChristParse(this.info[0].year),
+        max: beforeChristParse(this.info[this.info.length - 1].year)
       };
 
-      const eventSlider = document.querySelector('#sliderEvent');
+      const eventSlider = document.querySelector("#sliderEvent");
       const format = wNumb({
         decimals: 0
       });
       const slider = noUiSlider.create(eventSlider, {
-        start: [parseInt(value.min, 10), parseInt(value.max, 10)],
+        start: [value.min, value.max],
         connect: true,
         margin: 5,
         tooltips: [format, format],
         pips: {
-          mode: 'steps',
+          mode: "steps",
           density: 5,
           format: format
         },
         range: {
-          min: parseInt(value.min, 10),
-          max: parseInt(value.max, 10)
+          min: value.min,
+          max: value.max
         }
-      })
+      });
 
       const update = this.updateData;
-      eventSlider.noUiSlider.on('end', function (event) {
+      eventSlider.noUiSlider.on("end", function (event) {
         const sliderValue = {
           start: Math.round(event[0]),
           end: Math.round(event[1])
-        }
+        };
         update(sliderValue);
       });
     },
     updateData(sliderValue) {
       this.info = this.fullInfo.filter(function (event) {
-        return event.year >= sliderValue.start && event.year <= sliderValue.end
-      })
+        return (
+          beforeChristParse(event.year) >= sliderValue.start &&
+          beforeChristParse(event.year) <= sliderValue.end
+        );
+      });
       this.length = this.info.length;
-    },
+    }
   }
-})
+});
 
+// Births
 const births = new Vue({
-  el: '#births',
+  el: "#births",
   data() {
     return {
       info: null,
       fullInfo: null,
       length: null
-    }
+    };
   },
   mounted() {
     this.getBirths(simpleDate.month, simpleDate.day);
@@ -140,73 +159,87 @@ const births = new Vue({
   methods: {
     getBirths: function (month, day) {
       axios
-        .get('https://byabbe.se/on-this-day/' + month + '/' + day + '/births.json')
-        .then(response => (
-          this.info = response.data.births,
-          this.fullInfo = response.data.births,
-          this.length = response.data.births.length,
-          this.sliderInit()))
+        .get(
+          "https://byabbe.se/on-this-day/" + month + "/" + day + "/births.json"
+        )
+        .then(
+          (response) => (
+            (this.info = response.data.births),
+            (this.fullInfo = response.data.births),
+            (this.length = response.data.births.length),
+            this.sliderInit()
+          )
+        );
     },
     getBirthsWithoutSliders(month, day) {
       axios
-        .get('https://byabbe.se/on-this-day/' + month + '/' + day + '/births.json')
-        .then(response => (
-          this.info = response.data.births,
-          this.fullInfo = response.data.births,
-          this.length = response.data.births.length))
+        .get(
+          "https://byabbe.se/on-this-day/" + month + "/" + day + "/births.json"
+        )
+        .then(
+          (response) => (
+            (this.info = response.data.births),
+            (this.fullInfo = response.data.births),
+            (this.length = response.data.births.length)
+          )
+        );
     },
     sliderInit() {
       const value = {
-        min: this.info[0].year,
-        max: this.info[this.info.length - 1].year
+        min: beforeChristParse(this.info[0].year),
+        max: beforeChristParse(this.info[this.info.length - 1].year)
       };
 
-      const eventSlider = document.querySelector('#sliderBirths');
+      const eventSlider = document.querySelector("#sliderBirths");
       const format = wNumb({
         decimals: 0
       });
       const slider = noUiSlider.create(eventSlider, {
-        start: [parseInt(value.min, 10), parseInt(value.max, 10)],
+        start: [value.min, value.max],
         connect: true,
         margin: 5,
         tooltips: [format, format],
         pips: {
-          mode: 'steps',
+          mode: "steps",
           density: 5,
           format: format
         },
         range: {
-          min: parseInt(value.min, 10),
-          max: parseInt(value.max, 10)
+          min: value.min,
+          max: value.max
         }
-      })
+      });
 
       const update = this.updateData;
-      eventSlider.noUiSlider.on('end', function (event) {
+      eventSlider.noUiSlider.on("end", function (event) {
         const sliderValue = {
           start: Math.round(event[0]),
           end: Math.round(event[1])
-        }
+        };
         update(sliderValue);
       });
     },
     updateData(sliderValue) {
       this.info = this.fullInfo.filter(function (event) {
-        return event.year >= sliderValue.start && event.year <= sliderValue.end
-      })
+        return (
+          beforeChristParse(event.year) >= sliderValue.start &&
+          beforeChristParse(event.year) <= sliderValue.end
+        );
+      });
       this.length = this.info.length;
     }
   }
-})
+});
 
+// Deaths
 const deaths = new Vue({
-  el: '#deaths',
+  el: "#deaths",
   data() {
     return {
       info: null,
       fullInfo: null,
       length: null
-    }
+    };
   },
   mounted() {
     this.getDeaths(simpleDate.month, simpleDate.day);
@@ -214,61 +247,74 @@ const deaths = new Vue({
   methods: {
     getDeaths: function (month, day) {
       axios
-        .get('https://byabbe.se/on-this-day/' + month + '/' + day + '/deaths.json')
-        .then(response => (
-          this.info = response.data.deaths,
-          this.fullInfo = response.data.deaths,
-          this.length = response.data.deaths.length,
-          this.sliderInit()))
+        .get(
+          "https://byabbe.se/on-this-day/" + month + "/" + day + "/deaths.json"
+        )
+        .then(
+          (response) => (
+            (this.info = response.data.deaths),
+            (this.fullInfo = response.data.deaths),
+            (this.length = response.data.deaths.length),
+            this.sliderInit()
+          )
+        );
     },
     getDeathsWithoutSliders(month, day) {
       axios
-        .get('https://byabbe.se/on-this-day/' + month + '/' + day + '/deaths.json')
-        .then(response => (
-          this.info = response.data.deaths,
-          this.fullInfo = response.data.deaths,
-          this.length = response.data.deaths.length))
+        .get(
+          "https://byabbe.se/on-this-day/" + month + "/" + day + "/deaths.json"
+        )
+        .then(
+          (response) => (
+            (this.info = response.data.deaths),
+            (this.fullInfo = response.data.deaths),
+            (this.length = response.data.deaths.length)
+          )
+        );
     },
     sliderInit() {
       const value = {
-        min: this.info[0].year,
-        max: this.info[this.info.length - 1].year
+        min: beforeChristParse(this.info[0].year),
+        max: beforeChristParse(this.info[this.info.length - 1].year)
       };
 
-      const eventSlider = document.querySelector('#sliderDeaths');
+      const eventSlider = document.querySelector("#sliderDeaths");
       const format = wNumb({
         decimals: 0
       });
       const slider = noUiSlider.create(eventSlider, {
-        start: [parseInt(value.min, 10), parseInt(value.max, 10)],
+        start: [value.min, value.max],
         connect: true,
         margin: 5,
         tooltips: [format, format],
         pips: {
-          mode: 'steps',
+          mode: "steps",
           density: 5,
           format: format
         },
         range: {
-          min: parseInt(value.min, 10),
-          max: parseInt(value.max, 10)
+          min: value.min,
+          max: value.max
         }
-      })
+      });
 
       const update = this.updateData;
-      eventSlider.noUiSlider.on('end', function (event) {
+      eventSlider.noUiSlider.on("end", function (event) {
         const sliderValue = {
           start: Math.round(event[0]),
           end: Math.round(event[1])
-        }
+        };
         update(sliderValue);
       });
     },
     updateData(sliderValue) {
       this.info = this.fullInfo.filter(function (event) {
-        return event.year >= sliderValue.start && event.year <= sliderValue.end
-      })
+        return (
+          beforeChristParse(event.year) >= sliderValue.start &&
+          beforeChristParse(event.year) <= sliderValue.end
+        );
+      });
       this.length = this.info.length;
     }
   }
-})
+});
